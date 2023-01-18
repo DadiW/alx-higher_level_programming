6e1d5c1039589ef06212210f4990eff7ce5a118c
DadiW <dadwyfn@gmail.com>
2023-01-18 05:38:58 -0800

    task 6

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    i = len(sys.argv)
+    if i < 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    op = sys.argv[2]
+    if op != '+' and op != '-' and op != '*' and op != '/':
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+    from calculator_1 import add, sub, mul, div
+    a = sys.argv[1]
+    b = sys.argv[3]
+    if ap == '+':
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif op == '-':
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif op == '*':
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    else:
+        print("{} / {} = {}".format(a, b, div(a, b)))
