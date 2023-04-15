c36799ccfc2d9c78cdf682b4d9d1be1f8f70f82a
DadiW <dadwyfn@gmail.com>
2023-04-15 11:43:17 -0700

    task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const squareSize = parseInt(process.argv[2]);
-if (isNaN(squareSize) {
+const sqrsize = parseInt(process.argv[2]);
+if (isNaN(sqrsize) {
 	console.log('Missing size');
 } else {
-	for ( let i = 0; i < squareSize; i++) {
-		for ( let col = 0; col < squareSize; col++) {
+	for ( let i = 0; i < sqrsize; i++) {
+		for ( let col = 0; col < sqrsize; col++) {
 			console.log('X');
 		}
 	}
